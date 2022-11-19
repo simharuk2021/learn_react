debd4ff6725fd6b037947d6fe98d116dde32bc4e
Simharuk2021 <simharuk@yahoo.co.uk>
2022-11-19 03:07:28 +0000

    clickable ratings added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+import Rating from'./Rating';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>
+      My First React App
+      <Rating rating="1"/>
+      <Rating rating="2"/>
+      <Rating rating="3"/>
+      <Rating rating="4"/>
+      <Rating rating="5"/>
+      <Products />
+      <Button variant = "primary" disabled >Default</Button>
+      </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
