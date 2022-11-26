821509e54557a70ce2bec1938164a28627122019
Simharuk2021 <simharuk@yahoo.co.uk>
2022-11-26 19:59:00 +0000

    routes added (fixes implemented)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Products from './Products';
 import UserForm from './UserForm';
 // import Jumbotron from './Jumbotron';
 import Github from './GitHub';
 
 class App extends Component {
+    render() {
+      return (
+        <div>
+          <Header />
+        </div>
+      );
+    }
+}
+export default App;
+
+class Header extends Component {
   render() {        
     return (
+      <BrowserRouter>
       <div>
+        <Routes>
         {/* <UserForm /> */}
         {/* <Products />    */}
-        <Github />
+        <Route path ="/github" element = {<Github/>}/>
+        <Route exact path ="/" element = {<Home/>} />
+        <Route path ="/*" element = {<NotFound/>} />
+        </Routes>
       </div>
-    );
+      </BrowserRouter>)
   }
 }
 
-export default App;
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        Not Found
+      </div>
+    )
+  }
+}
 
